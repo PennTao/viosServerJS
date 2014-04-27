8beaab4a020a62d7e1bbb639d156f13caff0168a
Tao Lei <whu.eis.lt@gmail.com>
2014-04-26 22:06:42 -0400

    function extended, add user

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@
 var fs = require('fs');
 var path = require('path');
 var util = require('util');
+var crypto = require('crypto');
+var User = require('../models/User.js');
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index', {
@@ -81,4 +83,101 @@ module.exports = function (app) {
 	res.end(img, 'binary');
 
     });
+
+    app.get('/reg', checkNLogin);
+    app.get('/reg', function (req, res) {
+        res.render('reg', {
+            title: 'User Signup'
+        });
+    });
+
+    app.get('/reg', checkNLogin);
+    app.post('/reg', function (req, res) {
+        if (req.body['password-repeat'] != req.body['password']) {
+            req.flash('error', 'Two passwords are different');
+            return res.redirect('/reg');
+        }
+        var md5 = crypto.createHash('md5');
+        var password = md5.update(req.body.password).digest('base64');
+
+        var newUser = new User({
+            name: req.body.username,
+            password: password,
+        });
+        User.get(newUser.name, function (err, user) {
+            if (user)
+                err = 'Username already exists.';
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/reg');
+            }
+            newUser.save(function (err) {
+                if (err) {
+                    req.flash('error', err);
+                    return res.redirect('/reg');
+                }
+        
+                console.log(req.session.user);
+                req.flash('success', 'Register success');
+                res.redirect('/reg');
+            });
+        });
+    });
+
+	app.get('/login', checkNLogin);
+    app.get('/login', function (req, res) {
+        res.render('login', {
+            title: 'User Login',
+        });
+    });
+
+    app.get('/login', checkNLogin);
+    app.post('/login', function (req, res) {
+        var md5 = crypto.createHash('md5');
+        var password = md5.update(req.body.password).digest('base64');
+
+        User.get(req.body.username, function (err, user) {
+            if (!user) {
+                req.flash('error', 'user does not exist');
+                return res.redirect('/login');
+            }
+            if (user.password != password) {
+                req.flash('error', 'Wrong password!');
+                return res.redirect('/login');
+            }
+            req.session.user = user;
+            console.log(req.session.user);
+            req.flash('success', 'Log in successfully');
+            res.redirect('/u/' + req.session.user.name + '/profile');
+        });
+    });
+
+	app.get('/logout', checkLogin);
+    app.get('/logout', function (req, res) {
+        req.session.user = null;
+        req.flash('success', 'Log out successfully');
+        res.redirect('/');
+    });
+
+    app.get('/u/:user/profile',function(req,res){
+    	res.render('userpage', {
+            username: req.params.user
+        })
+
+
+    })
+    function checkLogin(req, res, next) {
+	    if (!req.session.user) {
+	        req.flash('error', 'Please log in first');
+	        return res.redirect('/login');
+	    }
+	    next();
+	};
+	function checkNLogin(req, res, next) {
+	    if (req.session.user) {
+	        req.flash('error', 'logged in already');
+	        return res.redirect('/u/' + req.session.user.name + '/page/1');
+	    }
+	    next();
+	};
 }
